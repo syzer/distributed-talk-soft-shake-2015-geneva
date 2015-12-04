7c4fca5f7274cb631d19fa1f8c765986016269ad
syzer3@gmail.com <syzer3@gmail.com>
2015-12-04 09:31:50 +0100

    es6 2

--- 2sendFileLikeABoss.js
+++ 2sendFileLikeABoss.js
@@ -1,5 +1,5 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const fs = require('fs');
 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'image/png'});
